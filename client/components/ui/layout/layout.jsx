a44b7a833a0e0501e4c4109dc29a74936a8e5e79
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2024-01-10 11:01:18 -0800

    added <head>

--- client/components/ui/layout/layout.jsx
+++ client/components/ui/layout/layout.jsx
@@ -5,6 +5,10 @@ import Navbar from "../navbar/navbar";
 import Footer from "../footer/footer";
 import { useRef } from "react";
 
+export const site_description =
+  "👋 Hello! I'm a full-stack web developer from Kelowna, BC! I'm a Software Developer specializing in building full-stack e-commerce web applications. ";
+
+export const site_title = "Andrew McMillan: Software Developer";
 const Layout = ({ children }) => {
   const containerRef = useRef();
   const handleMouseMove = (e) => {
@@ -16,15 +20,22 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Andrew McMillan's Portfolio" />
-        <meta
-          property="og:image"
-          content={`https://mcmillanfarms.xyz/api/og`}
-        />
-        <meta name="og:title" content="Andrew McMillan's Portfolio" />
+        <title>{site_title}</title>
+        <meta name="description" content={site_description} />
+        <meta property="og:title" content={site_title} />
+        <meta property="og:description" content={site_description} />
+        <meta property="og:image" content={"/images/og_image.png"} />
+        <meta property="og:url" content={"https://andrewrmcmillan.com"} />
+        <meta property="og:type" content="profile" />
         <meta name="twitter:card" content="summary_large_image" />
-        <title>Andrew McMillan</title>
+        <meta name="twitter:title" content={site_title} />
+        <meta name="twitter:site" content="@itsAndrew_dev" />
+        <meta name="twitter:description" content={site_description} />
+        <meta name="twitter:image" content={"/images/og_image.png"} />
+        <link rel="canonical" href={"https://andrewrmcmillan.com"} />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="robots" content="index, follow" />
       </Head>
       <div className="google-analytics-container">
         {/* <!-- Google tag (gtag.js) --> */}
