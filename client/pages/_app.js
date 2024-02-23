2a9ee0ac577bd4b0fe1d8affbc066701175c4979
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2024-02-22 19:22:26 -0800

    added analytics

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
-
+import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <GoogleAnalytics gaId="G-YW2MZW6N3V" />
       <GoogleTagManager gtmId="GTM-5FFLPW4Q" />
+      <Analytics />
     </>
   );
 }
